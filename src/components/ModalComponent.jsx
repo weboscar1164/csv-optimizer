e052b30fd643735932444351f20165433a0a0adf
webshoposcar1164 <cheaptrip1164@gmail.com>
2023-12-28 19:51:03 +0900

    表示修正 onEditData引数修正 モーダル内コンテンツ条件分岐を関数にまとめる

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -48,85 +48,97 @@ const ModalComponent = ({
 		setCurrentData(editedData);
 	};
 
+	const renderContent = () => {
+		if (showModal === "edit" && currentData) {
+			return (
+				<table>
+					<tbody>
+						<tr>
+							<td>氏名</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.name}
+									onChange={(e) => onEditData("name", e.target.value)}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>郵便番号</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.addressNum}
+									onChange={(e) => onEditData("addressNum", e.target.value)}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>住所1</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.address1}
+									onChange={(e) => onEditData("address1", e.target.value)}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>住所2</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.address2}
+									onChange={(e) => onEditData("address2", e.target.value)}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>電話番号</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.phone}
+									onChange={(e) => onEditData("phone", e.target.value)}
+								/>
+							</td>
+						</tr>
+						<tr>
+							<td>品名</td>
+							<td>
+								<input
+									type="text"
+									autoComplete="off"
+									value={currentData.product}
+									onChange={(e) => onEditData("product", e.target.value)}
+								/>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			);
+		} else if (showModal === "delete" && currentData) {
+			return (
+				<>
+					<p>{currentData.name}さんの</p>
+					<p>データを削除しますか?</p>
+				</>
+			);
+		} else {
+			return <p>ダウンロードしますか？</p>;
+		}
+	};
+
 	return (
 		<Modal isOpen={isOpen} contentLabel="Example Modal" style={customStyles}>
 			<div className="app-modal-container">
 				<h2>{title}</h2>
-
-				{showModal == "edit" && currentData ? (
-					<table>
-						<tbody>
-							<tr>
-								<td>氏名</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.name}
-										onChange={(e) => onEditData("name", e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>郵便番号</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.addressNum}
-										onChange={(e) => onEditData("name", e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>住所1</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.address1}
-										onChange={(e) => onEditData("address1", e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>住所2</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.address2}
-										onChange={(e) => onEditData("address2", e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>電話番号</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.phone}
-										onChange={(e) => onEditData("phone", e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>品名</td>
-								<td>
-									<input
-										type="text"
-										autoComplete="off"
-										value={currentData.product}
-										onChange={(e) => onEditData("product", e.target.value)}
-									/>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				) : (
-					<p>{content}</p>
-				)}
+				{renderContent()}
 				{showModal === "download" ? (
 					<CSVDownloader
 						data={convertData}
